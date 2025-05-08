5c78b5a062023288883e36eb2ef0569adefa5008
Zacharia Ndinguri <zacthuku@gmail.com>
2025-05-08 05:58:54 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+   sequence = []
+
+   if length >= 1:
+    sequence.append(0)
+   if length >= 2:
+    sequence.append(1)
+
+   for i in range(2,length):
+    sequence.append(sequence[-1]+sequence[-2])
+
+   print(sequence)
+
+   
